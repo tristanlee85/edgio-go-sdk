4206d07c5ad8c15122dad182bef377621894f797
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-04-01 17:29:18 -0300

    chore(ci-cd): added 'general' as a valid git summary scope [#25]

--- tools/scripts/validate-commit-msgs.sh
+++ tools/scripts/validate-commit-msgs.sh
@@ -2,7 +2,7 @@
 
 commitSummaryRegex="^(\+|\-)\s[a-z0-9]+\s(.+)$"
 commitTypeRegex="^(chore|docs|feat|fix|perf|ref|revert|style|test|wip)"
-commitScopeRegex="^[a-z]+(\((a-rule|a-schema|acl|app-scope|bot-cfg|bot-rule|bot|c-rule|cache|cdn|ci-cd|client|common|config|deployment|e-rule|env-var|env|internal|m-role|org|property|r-rule|tls|token|util)\))?\:\s"
+commitScopeRegex="^[a-z]+(\((a-rule|a-schema|acl|app-scope|bot-cfg|general|bot-rule|bot|c-rule|cache|cdn|ci-cd|client|common|config|deployment|e-rule|env-var|env|internal|m-role|org|property|r-rule|tls|token|util)\))?\:\s"
 issueNumberRegex="\s(\[#[0-9]+\]|\(#[0-9]+\))$"
 defectiveCommitSummaries=()
 
