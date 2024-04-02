92278d820dd9f2b176770186707208d4754ef1bb
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-04-02 13:37:28 -0300

    docs(util): added code-docs for GetByAttr func [#42]

--- internal/utils/get-by.go
+++ internal/utils/get-by.go
@@ -4,6 +4,11 @@ import (
 	"edgio/common"
 )
 
+// GetByAttr returns the first item in the haystack that matches the needle.
+// Mandatory params:
+// common.FilterListParams.Needle
+// common.FilterListParams.Haystack
+// Returns the first item that matches the needle or nil if no items match the needle.
 func GetByAttr[T common.Filterable](params common.FilterListParams[T]) T {
 	var result T
 
